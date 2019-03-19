6b12e90c122b2f4997ebd763c778ee663fe858c4
Hill <hill@kimo.com>
2019-03-20 03:13:50 +0800

    Add more output support

--- wpscan.sh
+++ wpscan.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-output=/log/
+dockerOutput=/log/
+output=/tmp/
 
 if [ -n "$2" ] ; then
+  dockerOutput+=$2
   output+=$2
 else
+  dockerOutput+=wpscan.log
   output+=wpscan.log
 fi
 
 echo "Output: $output"
+echo '' > $output
 
-docker run -it -v /tmp:/log --rm wpscanteam/wpscan -o $output --url $1
+docker run -it -v /tmp:/log --rm wpscanteam/wpscan -o $dockerOutput --url $1
+less -r $output
