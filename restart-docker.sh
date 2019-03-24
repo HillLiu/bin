98d5b02e83c2d74241d31b43866a2e1919ef00c0
hill <hill@kimo.com>
2019-03-24 19:35:04 +0000

    Add more notice

--- restart-docker.sh
+++ restart-docker.sh
@@ -4,8 +4,17 @@ if [ "$(whoami)" != "root" ]; then
     echo "Sorry, you are not root."
     exit 1
 fi
+
+echo "Stop all dokku--- Begin -->"
 sudo dokku ps:stopall
+echo "Stop all dokku--- End --/>"
 sleep 3;
+
+echo "Restart Docker --- Begin -->"
 systemctl restart docker.service
+echo "Restart Docker --- End --/>"
 sleep 3;
+
+echo "Start all dokku--- Begin -->"
 sudo dokku ps:startall
+echo "Start all dokku--- End --/>"
