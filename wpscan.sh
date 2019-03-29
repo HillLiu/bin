cc1b1713c66acf6bb05be5df618c371c402498e6
Hill <hill@kimo.com>
2019-03-29 22:15:30 +0800

    update wpscan

--- wpscan.sh
+++ wpscan.sh
@@ -2,17 +2,24 @@
 
 dockerOutput=/log/
 output=/tmp/
+scanUrl=$1
 
 if [ -n "$2" ] ; then
-  dockerOutput+=$2
-  output+=$2
+  logFile=$2
+  dockerOutput+=$logFile
+  output+=$logFile
 else
-  dockerOutput+=wpscan.log
-  output+=wpscan.log
+  hostname=`echo "$scanUrl" | awk -F/ '{print $3}'`
+  logFile=wpscan-${hostname}.log
+  dockerOutput+=$logFile
+  output+=$logFile
 fi
 
 echo "Output: $output"
 echo '' > $output
 
 docker run -it -v /tmp:/log --rm wpscanteam/wpscan -o $dockerOutput --url $1
+
+ansi2html $output $output.html
+
 less -r $output
