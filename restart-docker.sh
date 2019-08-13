5e05992c9d361274d91edfa2821c1f6ea3e6ba01
Hill <hill@kimo.com>
2019-08-13 08:48:41 +0000

    add iptables restore

--- restart-docker.sh
+++ restart-docker.sh
@@ -5,15 +5,25 @@ if [ "$(whoami)" != "root" ]; then
     exit 1
 fi
 
+IP_SAVE="/sbin/iptables-save"
+IP_RESTORE="/sbin/iptables-restore"
+IP_SAVE_FILE="/dev/shm/iptables.save"
+IPTABLES="/home/sys/bin/iptable-util"
+MY_HOST=`hostname | awk -F. '{print $1}'`
+MY_IPTABLES="/home/sys/bin/support/myip_${MY_HOST}.sh"
+
 echo "Stop all dokku--- Begin -->"
 sudo dokku ps:stopall
 echo "Stop all dokku--- End --/>"
 sleep 3;
 
+if [[ -x "$IPTABLES" && -x "$MY_IPTABLES" ]]; then
+	$IPTABLES --stop
+fi
+
 echo "Restart Docker --- Begin -->"
 systemctl restart docker.service
 echo "Restart Docker --- End --/>"
-sleep 3;
 
 echo "Start all dokku--- Begin -->"
 sudo dokku ps:startall
@@ -23,3 +33,15 @@ sleep 3;
 echo "Cleanup dokku--- Begin -->"
 sudo dokku cleanup
 echo "Cleanup dokku--- End --/>"
+
+if [ -x "$IP_SAVE" ]; then
+	$IP_SAVE > $IP_SAVE_FILE 
+fi
+
+if [ -x "$MY_IPTABLES" ]; then
+	$MY_IPTABLES 
+fi
+
+if [[ -e $IP_SAVE_FILE && -x $IP_RESTORE ]]; then
+	$IP_RESTORE < $IP_SAVE_FILE
+fi
