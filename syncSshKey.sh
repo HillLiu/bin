2f3ec683db2c592badfe4b5f7ec2ee0532f7e084
Hill <hill@kimo.com>
2022-05-21 14:49:11 +0000

    fixed path

--- syncSshKey.sh
+++ syncSshKey.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 remote=$1
+
+if [ -z "$remote" ]; then
+  echo "Need provide remote server";
+  exit 1
+fi
+
 ssh $remote mkdir -p ./.ssh
 scp $HOME/.ssh/authorized_keys $remote:./.ssh
-scp .bash_profile $remote:./
+scp $HOME/.bash_profile $remote:./
