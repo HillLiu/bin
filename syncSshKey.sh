d4a127c79fa1b8961b8d8aac90b6f0214deb618b
Hill <hill@kimo.com>
2022-11-06 19:38:20 +0800

    update

--- syncSshKey.sh
+++ syncSshKey.sh
@@ -7,6 +7,7 @@ if [ -z "$remote" ]; then
   exit 1
 fi
 
-ssh $remote mkdir -p ./.ssh
+ssh $remote mkdir -p ./.ssh ./.vim/autoload
 scp $HOME/.ssh/authorized_keys $remote:./.ssh
+scp $HOME/.vim/autoload/plug.vim $remote:./.vim/autoload/
 scp $HOME/.bash_profile $HOME/.vimrc $HOME/.screenrc $remote:./
